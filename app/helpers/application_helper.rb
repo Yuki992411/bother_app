f00bffe136c7f5be5ade4245d2ac338fa9e180a0
Yuki992411 <k.yuki.9924.11@gmail.com>
2021-09-24 05:00:08 +0900

    refactor(*) change style of nav-bar
    * position is fixed -> some scss files is changed
    * add person icon
    * position of nav-items is changed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,8 @@
 module ApplicationHelper
   def header_link_item(name, path)
-    class_name = 'nav-item'
+    class_name = 'nav-item navbar-brand mb-0 h1 font-weight-bold mr-auto'
     class_name << ' active' if current_page?(path)
 
-    content_tag :li, class: class_name do 
-      link_to name, path, class: 'nav-link'
-    end
+    link_to name, path, class: class_name
   end
 end
